fd573a5912b89a61f90dfa5d8a877298efb0ae71
kevinzhow <kevinchou.c@gmail.com>
2015-02-11 12:11:44 +0800

    Add Home and Home year cell

---
+++ Diary/Cell/YearCell/HomeYearCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  HomeYearCollectionViewCell.swift
+//  Diary
+//
+//  Created by kevinzhow on 15/2/11.
+//  Copyright (c) 2015年 kevinzhow. All rights reserved.
+//
+
+import UIKit
+
+class HomeYearCollectionViewCell: UICollectionViewCell {
+    
+}
