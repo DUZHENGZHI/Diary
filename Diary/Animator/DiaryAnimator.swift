6d001ab442200215318e409dcc077f38106c5653
kevinzhow <kevinchou.c@gmail.com>
2015-03-07 11:07:37 +0800

    Clean code

--- Diary/Animator/DiaryAnimator.swift
+++ Diary/Animator/DiaryAnimator.swift
@@ -40,31 +40,8 @@ class DiaryAnimator: NSObject, UIViewControllerAnimatedTransitioning {
         
         var initialRect = CGRectMake(fromVC!.view.frame.size.width/2.0, fromVC!.view.frame.size.height/2.0, 0, 0)
         
-        //inview.window?.convertRect(fromCollectionView!.frame, fromView: fromCollectionView?.superview)
-        
         var finalRect  = transitionContext.finalFrameForViewController(toVC!)
         
-//        var toLayout = toCollectionView?.collectionViewLayout as! UICollectionViewFlowLayout
-//        
-//        var currentLayout = fromCollectionView?.collectionViewLayout as! UICollectionViewFlowLayout
-//        var currentLayoutCopy = UICollectionViewFlowLayout.new()
-//        
-//        currentLayoutCopy.itemSize = currentLayout.itemSize
-//        currentLayoutCopy.sectionInset = currentLayout.sectionInset
-//        currentLayoutCopy.minimumLineSpacing = currentLayout.minimumLineSpacing
-//        currentLayoutCopy.minimumInteritemSpacing = currentLayout.minimumInteritemSpacing
-//        currentLayoutCopy.scrollDirection = currentLayout.scrollDirection
-//        
-//        self.fromCollectionView?.setCollectionViewLayout(currentLayoutCopy, animated: false)
-        
-//        var contentInset = toCollectionView.contentInset
-        
-//        var oldBottomInset = contentInset.bottom
-//        contentInset.bottom = CGRectGetHeight(finalRect)-(toLayout.itemSize.height+toLayout.sectionInset.bottom+toLayout.sectionInset.top)
-//        
-//        self.toCollectionView.contentInset = contentInset;
-//        self.toCollectionView.setCollectionViewLayout(currentLayout, animated: false)
-//        toView.frame = initialRect
 
         toView.alpha = 0.0
         
@@ -78,7 +55,6 @@ class DiaryAnimator: NSObject, UIViewControllerAnimatedTransitioning {
 
         UIView.animateWithDuration(transitionDuration(transitionContext), delay: 0, options: UIViewAnimationOptions.TransitionCrossDissolve, animations:
             {
-//                toView.frame = finalRect
                 if (self.pop) {
                     fromView.transform = CGAffineTransformMakeScale(3.3,3.3)
                 }else{
@@ -87,16 +63,6 @@ class DiaryAnimator: NSObject, UIViewControllerAnimatedTransitioning {
                 
                 toView.alpha = 1.0
 
-//                self.toCollectionView.performBatchUpdates({
-//                    self.toCollectionView.setCollectionViewLayout(toLayout, animated: false)
-//                }, completion: { finished in
-//                    
-//                    self.toCollectionView.contentInset = UIEdgeInsetsMake(contentInset.top,
-//                        contentInset.left,
-//                        oldBottomInset,
-//                        contentInset.right)
-//                })
-            
             }, completion: { finished in
                  transitionContext.completeTransition(true)
         })
